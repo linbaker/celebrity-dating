a0b545cc48fa99d6a9a5d788290086ba49d6f93a
AJ Ancheta and Lindsey Baker <aj@>
2019-03-28 16:30:38 -0700

    create alert with value dependent on radio inputs

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,22 @@ $(document).ready(function(){
     var iceCream = parseInt($("input:radio[name=iceCream]:checked").val());
     var vacation = parseInt($("input:radio[name=vacation]:checked").val());
     var total = add(time, iceCream, vacation);
-    alert(total);
 
+
+    if (total === 3){
+      alert("bob");
+    } else if (total === 9){
+      alert("joe");
+    } else {
+      alert("jill");
+};
     event.preventDefault();
-  });
+
 
 });
 
 var add = function(time, iceCream, vacation) {
   return time + iceCream + vacation;
-};
+  };
+
+});
